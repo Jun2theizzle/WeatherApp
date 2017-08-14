baa1b5537e10a72e4eb5dd26c188e6d3638d01cb
jun2theizzle <junnobaka@gmail.com>
2017-08-14 15:34:45 -0500

    added comments to weather module spec

--- tests/weatherModule.spec.js
+++ tests/weatherModule.spec.js
@@ -1,11 +1,15 @@
 describe('weatherModule', function(){
     var WeatherService;
+    
+    // Load the WeatherModule
     beforeEach(angular.mock.module('WeatherModule'));
 
+    // Inject our service
     beforeEach(inject(function(_WeatherService_) {
         WeatherService = _WeatherService_;
     }));
 
+    // These test are simple 'defined' tests as the module does 0 logic and only make http requests
     it('should exist', function() {
         expect(WeatherService).toBeDefined();
     });
@@ -14,4 +18,7 @@ describe('weatherModule', function(){
         expect(WeatherService.getFiveDayForecastByZipCode).toBeDefined();
     });
 
+    it('getFiveDayForecastByGeoLocation should exists', function() {
+        expect(WeatherService.getFiveDayForecastByGeoLocation).toBeDefined();
+    });
 });
